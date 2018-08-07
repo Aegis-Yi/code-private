d0530e809c0a3cbe8bffd74227f76a309ad028f1
Baka9 <15558193901@163.com>
2018-08-08 02:31:08 +0800

    old

---
+++ Csharp/scriptcs.cs
@@ -0,0 +1,5 @@
+int i = 0;
+int a = i++;
+int b = ++i;
+Console.WriteLine (a);
+Console.WriteLine (b);
---
+++ vscode/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+int main()
+{
+    using namespace std;
+    printf_s("hello world\n");
+    cout << "hello world\n";
+    return puts("hello world");
+    return printf("hello world");
+}
