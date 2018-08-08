bdf7de6e953c7bb0fdae7c120ac7b8799eac32c6
Baka9 <15558193901@163.com>
2018-08-08 09:23:28 +0800

    delet

--- vscode/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-#include <stdlib.h>
-int main()
-{
-    using namespace std;
-    printf_s("hello world\n");
-    return puts("hello world");
-    return printf("hello world");
-}
