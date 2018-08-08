e213258462a31e8a04f10b4fd37357add43935da
Baka9 <15558193901@163.com>
2018-08-08 09:07:23 +0800

    e

--- vscode/hello.cpp
+++ vscode/hello.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+#include "stdio.h"
 #include <stdlib.h>
 int main()
 {
     using namespace std;
     printf_s("hello world\n");
-    cout << "hello world\n";
     return puts("hello world");
     return printf("hello world");
 }
