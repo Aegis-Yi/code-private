be15d912169280fd0d63563891400b223f243787
宅丶格瓦拉 <15558193901@163.com>
2019-04-02 19:02:41 +0800

    C#接口反射

--- Csharp/dotnet/Program.cs
+++ Csharp/dotnet/Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
-using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
 interface Iop {
     void Test ();
 }
+
 class op1 : Iop {
     public void Test () => Console.WriteLine ("hello");
 }
 class op2 : Iop {
     public void Test () => Console.WriteLine ("world");
 }
-
+class op {
+    private Iop _op;
+    public op (Iop Op) => _op = Op;
+    public void optest () => _op.Test();
+}
 class Program {
     static void Main (string[] args) {
-        var sc = new SevericeCollection ();
-        sc.AddScoped (typeof (Iop), typeof (op1));
+        var sc = new ServiceCollection ();
+        sc.AddScoped (typeof (Iop), typeof (op2));
+        sc.AddScoped<op> ();
         var sp = sc.BuildServiceProvider ();
         //==================================//
-        Iop a = sp.GetSeverice<Iop> ();
-        a.Test ();
+        var a = sp.GetService<op> ();
+        a.optest ();
     }
 }
