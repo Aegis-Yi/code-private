198b22f59eaf155c6876c36b0c476112953c5d3b
“tes <15558193901@163.com>
2019-04-18 15:52:38 +0800

    xunit

--- Csharp/Xunit/Demo.Test/UnitTest1.cs
+++ Csharp/Xunit/Demo.Test/UnitTest1.cs
@@ -1,26 +1,33 @@
 using System;
+using Moq;
 using Xunit;
 namespace Demo.Test {
     public class DeskfanTest {
         [Fact]
         public void PowerLowerThanZero_OK () {
-        var fan = new Deskfan (new PowerLowerThanZero ());
+            var mock = new Mock<Ipower> ();
+            mock.Setup (power => power.GetPower ()).Returns (() => 0);
+            //var fan = new Deskfan (new PowerLowerThanZero ());
+            var fan = new Deskfan (mock.Object);
             var excepted = "don't work.";
             var actual = fan.Work ();
             Assert.Equal (excepted, actual);
         }
+
+        [Fact]
+        public void PowerLowerThanZero_boom () {
+            var fan = new Deskfan (new PowerLowerThanboom ());
+            var excepted = "boom!!!";
+            var actual = fan.Work ();
+            Assert.Equal (excepted, actual);
+        }
+
         class PowerLowerThanZero : Ipower {
             public int GetPower () {
                 return 0;
             }
         }
-        [Fact]
-        public void PowerLowerThanZero_boom () {
-        var fan = new Deskfan (new PowerLowerThanboom ());
-            var excepted = "boom!!!";
-            var actual = fan.Work ();
-            Assert.Equal (excepted, actual);
-        }
+
         class PowerLowerThanboom : Ipower {
             public int GetPower () {
                 return 300;
