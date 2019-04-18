98c2f7b2083602a36077cc448bb0fc23a9da6915
“tes <15558193901@163.com>
2019-04-18 15:38:39 +0800

    xunit

--- Csharp/Xunit/Demo.Main/Class1.cs
+++ Csharp/Xunit/Demo.Main/Class1.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 
-namespace Demo.Main {
-    public class Class1 {
-        public int GetNum (int Num) {
-            return Num;
+public interface Ipower {
+    int GetPower ();
+}
+public class PowerSupply : Ipower {
+    public int GetPower () {
+        return 100;
+    }
+}
+public class Deskfan {
+    private Ipower _PowGet;
+    public Deskfan (Ipower PowGet) {
+        _PowGet = PowGet;
+    }
+    public string Work () {
+        int power = _PowGet.GetPower ();
+        if (power <= 0) {
+            return "don't work.";
+        } else if (power < 100) {
+            return "work slow.";
+        } else if (power < 200) {
+            return "work hight.";
+        } else {
+            return "boom!!!";
+
         }
+
     }
 }
--- Csharp/Xunit/Demo.Test/UnitTest1.cs
+++ Csharp/Xunit/Demo.Test/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
-
-namespace Demo.Test
-{
-    public class UnitTest1
-    {
+namespace Demo.Test {
+    public class DeskfanTest {
         [Fact]
-        public void Test1()
-        {
-
+        public void PowerLowerThanZero_OK () {
+        var fan = new Deskfan (new PowerLowerThanZero ());
+            var excepted = "don't work.";
+            var actual = fan.Work ();
+            Assert.Equal (excepted, actual);
+        }
+        class PowerLowerThanZero : Ipower {
+            public int GetPower () {
+                return 0;
+            }
+        }
+        [Fact]
+        public void PowerLowerThanZero_boom () {
+        var fan = new Deskfan (new PowerLowerThanboom ());
+            var excepted = "boom!!!";
+            var actual = fan.Work ();
+            Assert.Equal (excepted, actual);
+        }
+        class PowerLowerThanboom : Ipower {
+            public int GetPower () {
+                return 300;
+            }
         }
     }
 }
