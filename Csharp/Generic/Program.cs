2dc537c13b296b969204aabe257a2699f6799d5c
“tes <15558193901@163.com>
2019-04-18 23:31:44 +0800

    泛型

--- Csharp/Generic/Program.cs
+++ Csharp/Generic/Program.cs
@@ -25,6 +25,7 @@ namespace Generic {
     }   
 }
 //*/
+/*
 namespace IoGeneric {
     interface IPeople<Tid> {
         Tid ID { get; set; }
@@ -44,3 +45,61 @@ namespace IoGeneric {
         }
     }
 }
+//*/
+/* 
+namespace ArGeneric {
+    class Program {
+        static void Main () {
+            int[] a1 = { 1, 2, 3, 4, 5 };
+            int[] b1 = { 1, 2, 3, 4, 5, 6 };
+            double[] a2 = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            double[] b2 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+            var result = Zip (a1, b1);
+            Console.WriteLine (string.Join (",", result));
+        }
+
+        static T[] Zip<T> (T[] a, T[] b) {
+            T[] zipped = new T[a.Length + b.Length];
+            int ai = 0, bi = 0, zi = 0;
+            do {
+                if (ai < a.Length) zipped[zi++] = a[ai++];
+                if (bi < b.Length) zipped[zi++] = b[bi++];
+            } while (ai < a.Length || bi < b.Length);
+            return zipped;
+        }
+    }
+} 
+//*/
+/* 
+namespace AcGeneric {
+    class Program {
+        static void Main () {
+            Action<string> a1 = Say;
+            a1 ("world");
+            Action<int> a2 = Num;
+            a2 (1);
+        }
+        static void Say (string str) {
+            Console.WriteLine ($"hello,{str}!");
+        }
+        static void Num (int x) {
+            Console.WriteLine (x * 100);
+        }
+    }
+} 
+//*/
+/* 
+namespace FuncGeneric {
+    class Program {
+        static void Main () {
+
+            Func<int, int, int> func1 = (a, b) => { return a + b; };
+            Func<double, double, double> func2 = (a, b) => { return a + b; };
+            var result1 = func1 (100, 220);
+            var result2 = func2 (100.1, 220.2);
+            Console.WriteLine (result1);
+            Console.WriteLine (result2);
+        }
+    }
+} 
+//*/
