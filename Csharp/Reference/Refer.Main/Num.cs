65f163bb6b8274c958a2bb36e6be99869be1f9bc
“tes <15558193901@163.com>
2019-04-23 15:30:28 +0800

    Refer

---
+++ Csharp/Reference/Refer.Main/Num.cs
@@ -0,0 +1,8 @@
+namespace nameNum {
+    class Num {
+        public int Number(int a, int b) {
+            var c = a + b;
+            return c;
+        }
+    }
+}
---
+++ Csharp/Reference/Refer.Main/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using nameNum;
+
+namespace Refer.Main {
+    class Program {
+        static void Main (string[] args) {
+            int a = 1, b = 2;
+            Num num = new Num ();
+            var result = num.Number (a, b);
+            Console.WriteLine (result);
+        }
+    }
+}
